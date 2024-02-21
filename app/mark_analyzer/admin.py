d46168cd53dcebd088b72034979465e243dafad7
dlobankov <lobankov082000@gmail.com>
2024-02-22 00:57:53 +0300

    make models

--- app/mark_analyzer/admin.py
+++ app/mark_analyzer/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from models import Article, Marks, Expert
 
 # Register your models here.
+admin.site.register(Article)
+admin.site.register(Marks)
+admin.site.register(Expert)
+
--- app/mark_analyzer/models.py
+++ app/mark_analyzer/models.py
@@ -1,3 +1,154 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Expert(models.Model):
+    uid = models.AutoField(primary_key=True, verbose_name='Номер')
+    name = models.CharField(null=False, max_length=64, verbose_name='Имя')
+    surname =  models.CharField(null=False, max_length=64, verbose_name='Фамилия, Отчество')
+
+    def __str__(self) -> str:
+        return f"{self.name} {self.surname}"
+    
+    class Meta:
+        verbose_name = 'Эксперт'
+        verbose_name_plural = 'Эксперты'
+
+
+
+class Article(models.Model):
+    IN_PROCESS = "--"
+    AGREED = "ОК"
+    EXPERTISE = "ДЭ"
+    ARTICLE_STATUS_CHOISES = {
+        IN_PROCESS: "В процессе",
+        AGREED: "Согласовано",
+        EXPERTISE: "Дополнительная экспертиза",
+    }
+    
+    uid = models.AutoField(primary_key=True, verbose_name='Номер')
+    title = models.CharField(null=False, max_length=256, verbose_name='Название')
+    author_full_name = models.CharField(null=False, max_length=64, verbose_name='Автор')
+    status = models.CharField(max_length=2, choices=ARTICLE_STATUS_CHOISES, default=IN_PROCESS, verbose_name='Статус')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
+
+    def __str__(self) -> str:
+        return f'"{self.title}"'
+    
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+
+
+class Marks(models.Model):
+    uid = models.AutoField(primary_key=True, verbose_name='Номер')
+    expert_id = models.ForeignKey(Expert, on_delete=models.PROTECT, verbose_name='Эксперт')
+    article_id = models.ForeignKey(Article, on_delete=models.PROTECT, verbose_name='Статья')
+    pni_1 = models.IntegerField(
+        null=False,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ],
+        verbose_name='ПНИ_1',
+        help_text='Соответствие предмета доклада тематике конференции',
+    )
+    pni_2 = models.IntegerField(
+        null=False,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ],
+        verbose_name='ПНИ_2',
+        help_text='Научная новизна представляемого материала',
+    )
+    pni_3 = models.IntegerField(
+        null=False,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ],
+        verbose_name='ПНИ_3',
+        help_text='Актуальность исследования',
+    )
+    pni_4 = models.IntegerField(
+        null=False,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ],
+        verbose_name='ПНИ_4',
+        help_text='Обоснованность применяемых методов исследования',
+    )
+    pni_5 = models.IntegerField(
+        null=False,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ],
+        verbose_name='ПНИ_5',
+        help_text='Обоснованность структуры статьи',
+    )
+    pni_6 = models.IntegerField(
+        null=False,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ],
+        verbose_name='ПНИ_6',
+        help_text='Обоснованность и наглядность представленных рисунков, графиков и таблиц',
+    )
+    po_1 = models.IntegerField(
+        null=False,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ],
+        verbose_name='ПО_1',
+        help_text='Соответствие оформленных материалов заданному шаблону',
+    )
+    po_2 = models.IntegerField(
+        null=False,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ],
+        verbose_name='ПО_2',
+        help_text='Качество языка изложения',
+    )
+    po_3 = models.IntegerField(
+        null=False,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ],
+        verbose_name='ПО_3',
+        help_text='Соответствие аннотации описанию работы',
+    )
+    po_4 = models.IntegerField(
+        null=False,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ],
+        verbose_name='ПО_4',
+        help_text='Обоснованность и качество ключевых слов ',
+    )
+    po_5 = models.IntegerField(
+        null=False,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ],
+        verbose_name='ПО_5',
+        help_text='Список литературы: адекватность и правильность цитирования',
+    )
+
+    def __str__(self) -> str:
+        return f'ПНИ_1:{self.pni_1}, ПНИ_2:{self.pni_2}, ПНИ_3:{self.pni_3}, ПНИ_4:{self.pni_4},
+          ПНИ_5:{self.pni_5}, ПНИ_6:{self.pni_6}, ПО_1:{self.po_1}, ПО_2:{self.po_2}, ПО_3:{self.po_3}, 
+          ПО_4:{self.po_4}, ПО_5:{self.po_5}.'
+    
+    class Meta:
+        verbose_name = 'Оценка'
+        verbose_name_plural = 'Оценки'
